c181a6ba7c9d511b86a90b88f7edc63c17d5b23f
Samim Aktar <samimaktar0773@gmail.com>
2023-11-29 23:33:56 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import express, { urlencoded } from "express";
+import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
@@ -15,5 +15,12 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// routes import
+
+import userRouter from "./routes/user.routes.js";
+
+// routes declaraton
+app.use("/api/v1/users", userRouter);
 
 export { app };
